bd03e97b0f5ac8a02c691398444cd5ccea840cf0
scribeNcode <deyeye99@gmail.com>
2024-05-22 13:56:45 +0100

    function to calculate grade

--- control-flow/Components/assignment.jsx
+++ control-flow/Components/assignment.jsx
@@ -20,4 +20,24 @@ export function findLargest(a,b,c){
     console.log(highestNum)
 }
 
+// Write a function calculateGrade that takes a number score (0-100) and returns a grade (A, B, C, D, F) based on the following scale:
+// A: 90-100, B: 80-89, C: 70-79, D: 60-69, F: 0-59.
+
+export function calculateGrade(num){
+    if(num >= 0 && num <= 59){
+        console.log('F')
+    }else if(num >= 60 && num <= 69){
+        console.log('D')
+    }else if(num >= 70 && num <= 79){
+        console.log('C')
+    }else if(num >= 80 && num <= 89){
+        console.log('B')
+    }else if(num >= 90 && num <= 100){
+        console.log('A')
+    }else{
+        console('not a valid grade')
+    }
+}
+
+
 
--- control-flow/src/App.jsx
+++ control-flow/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {numPrinter, findLargest} from '../Components/assignment'
+import {numPrinter, findLargest, calculateGrade} from '../Components/assignment'
 
 function App() {
-  console.log(numPrinter())
-  console.log(findLargest(54, 65, 43))
+  console.log(numPrinter());
+  console.log(findLargest(54, 65, 43));
+  console.log(calculateGrade(78))
   return (
     <>
       <div>App</div>
