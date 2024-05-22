792da265665119fb0f2c126662b1237cf4af1b06
scribeNcode <deyeye99@gmail.com>
2024-05-22 12:37:01 +0100

    conditionally print numbers from 1 to 50

---
+++ control-flow/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {numPrinter} from '../Components/assignment'
+
+function App() {
+  console.log(numPrinter())
+
+  return (
+    <>
+      <div>App</div>
+      
+    </>
+  )
+}
+
+export default App
